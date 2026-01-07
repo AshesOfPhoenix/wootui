37b553962f1f2ac6809ebbb3bfd52b8bfb978795
Kristjan Krizman <kristjan.krizman@gmail.com>
2026-01-07 17:34:04 +0100

    style: static images

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
 
     themeConfig: {
         // Replace with your project's social card
-        image: 'img/docusaurus-social-card.jpg',
+        image: 'img/wootui-social-card.jpg',
         colorMode: {
             respectPrefersColorScheme: true,
         },
